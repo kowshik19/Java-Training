fb36944d061d228f5c94724d306ca9f65f37009f
kowshik <kowshik1910@gmail.com>
2024-02-20 09:25:41 +0530

    .

---
+++ Day 1/Distance.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = 3;
+        int y1 = 4;
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        double dis = Math.sqrt((Math.pow(x2 - x1, 2)) + Math.pow(y2 - y1, 2));
+        System.out.println(" The Distance is " + Math.abs((int) dis));
+        sc.close();
+    }
+}
